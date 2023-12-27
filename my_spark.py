69cacccc6ecea6efaadb1cb6d6dff10af572d42f
naliab <naliab@yandex.ru>
2023-12-27 15:38:28 +0300

    feat: запрос переделан в соответствие с заданием

--- my_spark.py
+++ my_spark.py
@@ -1,26 +1,46 @@
 import sys
-#import psycopg2
-#from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession, SQLContext
 
 def main():
     spark = SparkSession \
         .builder \
-        .appName("Mean house price app") \
-        .master("spark://spark-master:7077") \
-        .config("spark.jars", "/jdbc.jar") \
+        .appName('Mean house price app') \
+        .master('spark://spark-master:7077') \
+        .config('spark.jars', '/jdbc.jar') \
         .getOrCreate()
 
     df = spark.read \
-        .format("jdbc") \
-        .option("url", "jdbc:postgresql://db:5432/spark") \
-        .option("dbtable", "house_prices") \
-        .option("user", "spark_user") \
-        .option("password", "spark_password") \
-        .option("driver", "org.postgresql.Driver") \
+        .format('jdbc') \
+        .option('url', 'jdbc:postgresql://db:5432/spark') \
+        .option('dbtable', 'house_prices') \
+        .option('user', 'spark_user') \
+        .option('password', 'spark_password') \
+        .option('driver', 'org.postgresql.Driver') \
         .load()
 
-    df.printSchema()
+    df.createOrReplaceTempView('house_prices')
 
-if __name__ == "__main__":
+    spark.sql('''
+        WITH cte AS (
+            SELECT
+                *, baths + bedrooms AS num_rooms
+            FROM
+                house_prices
+        )
+        SELECT
+            city AS City,
+            location AS Area,
+            num_rooms AS Num_of_rooms,
+            format_number(AVG(price), 2) AS house_and_flat_prices
+        FROM
+            cte
+        WHERE
+            property_type IN ("House", "Flat")
+        GROUP BY
+            city, location, num_rooms
+        ORDER BY
+            city, location, num_rooms
+    ''').show(df.count())
+
+if __name__ == '__main__':
         main()
